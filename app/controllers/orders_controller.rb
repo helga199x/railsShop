9d59e01d71c5fad6ca70ae2ec940fe72b6ab473c
alexei_kapskiy <kapskyalexei@gmail.com>
2021-07-21 22:23:13 +0300

    added client in admin page/fix several bags

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,34 +1,41 @@
 class OrdersController < ApplicationController
 
     def show
-        @finalOrderArray = []
-        iI = 1
-        while ((@orderHash = Order.all.where(order_number: iI)[0]) != nil)
-            if (@orderHash.status == "done")
+        if Order.maximum('order_number') == nil
+            @finalOrderArray = []
+        else
+            @finalOrderArray = []
+            iI = 1
+            while Order.maximum('order_number') >= iI
+                if (Order.all.where(order_number: iI) == nil || Order.all.where(order_number: iI) == [])
+                    iI += 1
+                    next
+                elsif (@orderHash = Order.all.where(order_number: iI)[0].status == "done")
+                    iI += 1
+                    next  
+                end
+                @orderHash = Order.all.where(order_number: iI)
+                @clients_db = Client.all.where(id: @orderHash[0].client_id)
+                @finalOrder = {
+                    client: [@clients_db[0].name, @clients_db[0].adress, @clients_db[0].phone],
+                    orderNumber: @orderHash[0].order_number,
+                    createdAt: @orderHash[0].created_at,
+                    status: @orderHash[0].status,
+                    totalPrice: [],
+                    good: [],
+                    amount: []
+                }
+                i = 0
+                while i < @orderHash.length
+                    @goods_db = Good.all.where(id: @orderHash[i].good_id)
+                    @finalOrder[:totalPrice].push(@orderHash[i].amount.to_i * @orderHash[i].price.to_i)
+                    @finalOrder[:good].push(@goods_db[0].name)
+                    @finalOrder[:amount].push(@orderHash[i].amount)
+                    i += 1
+                end
+                @finalOrderArray.push(@finalOrder)
                 iI += 1
-                next  
             end
-            @orderHash = Order.all.where(order_number: iI)
-            @clients_db = Client.all.where(id: @orderHash[0].client_id)
-            @finalOrder = {
-                client: [@clients_db[0].name, @clients_db[0].adress, @clients_db[0].phone],
-                orderNumber: @orderHash[0].order_number,
-                createdAt: @orderHash[0].created_at,
-                status: @orderHash[0].status,
-                totalPrice: [],
-                good: [],
-                amount: []
-            }
-            i = 0
-            while i < @orderHash.length
-                @goods_db = Good.all.where(id: @orderHash[i].good_id)
-                @finalOrder[:totalPrice].push(@orderHash[i].amount.to_i * @orderHash[i].price.to_i)
-                @finalOrder[:good].push(@goods_db[0].name)
-                @finalOrder[:amount].push(@orderHash[i].amount)
-                i += 1
-            end
-            @finalOrderArray.push(@finalOrder)
-            iI += 1
         end
     end
 
--- app/models/good.rb
+++ app/models/good.rb
@@ -1,3 +1,4 @@
 class Good < ApplicationRecord
     has_one_attached :storage_image
+    has_many :orders, dependent: :destroy
 end
