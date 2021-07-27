1170581f1fd78984925b65197b35ac24722ec3f1
AlexeiKap <kapskyalexei@gmail.com>
2021-07-27 22:39:30 +0300

    add devise gem/rspec gem

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,7 @@
 class Order < ApplicationRecord
     validates :good_id, :amount, :price, :client_id, presence: true
     validates :good_id, :client_id, :price, :amount, numericality: { greater_than: 0 }
+
     belongs_to :good
     belongs_to :client
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  devise_for :users
   root 'main#index'
   get 'admin' => 'admins#admin'
   get 'admin/orders' => 'admins#orders'
